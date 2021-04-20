a09a9d30d7e3ae15e85b96b3e69f50b539cb686d
ahmdtawfiek <ahmdtawfiek@gmail.com>
2021-04-20 02:37:51 +0200

    orders - complaints

---
+++ complaints.php
@@ -0,0 +1,75 @@
+<?php
+    include 'controllers/upper-header.php';
+?>
+<?php
+    include 'controllers/header.php';
+?>
+<nav aria-label="breadcrumb" class="breadcrumb">
+     <div class="container">
+         <ol class="breadcrumb mb-0">
+             <li class="breadcrumb-item"><a href="index.php">الرئيسيه</a></li>
+             <li class="breadcrumb-item active" aria-current="page">الشكاوى</li>
+         </ol>
+     </div>
+</nav>
+<div class="container mb-60 mt-30">
+    <div class="bordered">
+        <div class="row">
+            <div class="col-sm-4">
+                <article class="blue-complain">
+                    <h5><strong>رقم الشكوى</strong></h5>
+                    <p><small>123 4567 3454</small></p>
+                </article>            
+            </div>
+            <div class="col-sm-4">
+                <article class="green-complain">
+                    <h5><strong>حالة الشكوى</strong></h5>
+                    <p><small>123 4567 3454</small></p>
+                </article>            
+            </div>
+            <div class="col-sm-4">
+                <article class="blue-complain">
+                    <h5><strong>اسم العاملة</strong></h5>
+                    <p><small>123 4567 3454</small></p>
+                </article>            
+            </div>
+            <div class="col-sm-12 mt-30">
+                <article>
+                    <h5 class="blue-complain"><strong>تفاصيل الشكوى</strong></h5>
+                    <p class="bordered"><small>هذا النص هو مثال هذا النص هو مثال هذا النص هو مثال هذا النص هو مثال هذا النص هو مثال هذا النص هو مثال هذا النص هو مثال هذاالنص هو مثال هذا النص هو مثال هذاالنص هو مثال هذا النص هو مثال هذا النص هو مثال </small></p>
+                </article>            
+            </div>
+        </div>
+    </div>
+    <div class="bordered mt-30">
+        <div class="row">
+            <div class="col-sm-4">
+                <article class="blue-complain">
+                    <h5><strong>رقم الشكوى</strong></h5>
+                    <p><small>123 4567 3454</small></p>
+                </article>            
+            </div>
+            <div class="col-sm-4">
+                <article class="green-complain">
+                    <h5><strong>حالة الشكوى</strong></h5>
+                    <p><small>123 4567 3454</small></p>
+                </article>            
+            </div>
+            <div class="col-sm-4">
+                <article class="blue-complain">
+                    <h5><strong>اسم العاملة</strong></h5>
+                    <p><small>123 4567 3454</small></p>
+                </article>            
+            </div>
+            <div class="col-sm-12 mt-30">
+                <article>
+                    <h5 class="blue-complain"><strong>تفاصيل الشكوى</strong></h5>
+                    <p class="bordered"><small>هذا النص هو مثال هذا النص هو مثال هذا النص هو مثال هذا النص هو مثال هذا النص هو مثال هذا النص هو مثال هذا النص هو مثال هذاالنص هو مثال هذا النص هو مثال هذاالنص هو مثال هذا النص هو مثال هذا النص هو مثال </small></p>
+                </article>            
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+    include 'controllers/footer.php';
+?>
---
+++ orders.php
@@ -0,0 +1,130 @@
+<?php
+    include 'controllers/upper-header.php';
+?>
+<?php
+    include 'controllers/header.php';
+?>
+<nav aria-label="breadcrumb" class="breadcrumb">
+     <div class="container">
+         <ol class="breadcrumb mb-0">
+             <li class="breadcrumb-item"><a href="index.php">الرئيسيه</a></li>
+             <li class="breadcrumb-item active" aria-current="page">الطلبات</li>
+         </ol>
+     </div>
+</nav>
+<div class="container mb-60 mt-30">
+    <div class="bordered">
+        <div class="row">
+            <div class="col-md-4 col-sm-6 col-xs-6 mb-20">
+                <article class="blue-complain">
+                    <h5><strong>رقم الطلب</strong></h5>
+                    <p><small>123 4567 3454</small></p>
+                </article>            
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 mb-20">
+                <article class="green-complain">
+                    <h5><strong>اسم العاملة</strong></h5>
+                    <p><small>فتحيه</small></p>
+                </article>            
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 mb-20">
+                <article class="blue-complain">
+                    <h5><strong>حالة الطلب</strong></h5>
+                    <p><small>جديد</small></p>
+                </article>            
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 mb-20">
+                <article class="blue-complain">
+                    <h5><strong>نوع الطلب</strong></h5>
+                    <p><small>خدمى</small></p>
+                </article>            
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 mb-20">
+                <article class="blue-complain">
+                    <h5><strong>المهنه</strong></h5>
+                    <p><small>عامله</small></p>
+                </article>            
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 mb-20">
+                <button class="btn btn-primary mt-20 mb-20 radius-20">التفاصيل</button>       
+            </div>
+        </div>
+    </div>
+    <div class="bordered mt-30">
+        <div class="row">
+            <div class="col-md-4 col-sm-6 col-xs-6 mb-20">
+                <article class="blue-complain">
+                    <h5><strong>رقم الطلب</strong></h5>
+                    <p><small>123 4567 3454</small></p>
+                </article>            
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 mb-20">
+                <article class="green-complain">
+                    <h5><strong>اسم العاملة</strong></h5>
+                    <p><small>فتحيه</small></p>
+                </article>            
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 mb-20">
+                <article class="blue-complain">
+                    <h5><strong>حالة الطلب</strong></h5>
+                    <p><small>جديد</small></p>
+                </article>            
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 mb-20">
+                <article class="blue-complain">
+                    <h5><strong>نوع الطلب</strong></h5>
+                    <p><small>خدمى</small></p>
+                </article>            
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 mb-20">
+                <article class="blue-complain">
+                    <h5><strong>المهنه</strong></h5>
+                    <p><small>عامله</small></p>
+                </article>            
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 mb-20">
+                <button class="btn btn-primary mt-20 mb-20 radius-20">التفاصيل</button>       
+            </div>
+        </div>
+    </div>
+    <div class="bordered mt-30">
+        <div class="row">
+            <div class="col-md-4 col-sm-6 col-xs-6 mb-20">
+                <article class="blue-complain">
+                    <h5><strong>رقم الطلب</strong></h5>
+                    <p><small>123 4567 3454</small></p>
+                </article>            
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 mb-20">
+                <article class="green-complain">
+                    <h5><strong>اسم العاملة</strong></h5>
+                    <p><small>فتحيه</small></p>
+                </article>            
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 mb-20">
+                <article class="blue-complain">
+                    <h5><strong>حالة الطلب</strong></h5>
+                    <p><small>جديد</small></p>
+                </article>            
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 mb-20">
+                <article class="blue-complain">
+                    <h5><strong>نوع الطلب</strong></h5>
+                    <p><small>خدمى</small></p>
+                </article>            
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 mb-20">
+                <article class="blue-complain">
+                    <h5><strong>المهنه</strong></h5>
+                    <p><small>عامله</small></p>
+                </article>            
+            </div>
+            <div class="col-md-4 col-sm-6 col-xs-6 mb-20">
+                <button class="btn btn-primary mt-20 mb-20 radius-20">التفاصيل</button>       
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+    include 'controllers/footer.php';
+?>
